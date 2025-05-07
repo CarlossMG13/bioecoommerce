bf4b65b78d8ccc8b154e6cc44c09b5d3e049b5db
Carlos Morales García <116101975+CarlossMG13@users.noreply.github.com>
2025-05-06 18:11:02 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+
+const botonesCapsulas = document.querySelectorAll('.selector-capsulas button');
+const imagenProducto = document.getElementById('imagen-producto');
+const nombreProducto = document.getElementById('nombre-producto');
+const precioProducto = document.getElementById('precio-producto');
+
+let menuMobile = document.getElementById("menu-mobile");
+      let ham_menu = document.getElementById("ham_menu");
+
+      menuMobile.addEventListener("click", function () {
+        ham_menu.classList.toggle("hidden");
+      });
+      
+//Acordion
+
--- main.js
+++ main.js
@@ -1,16 +1,7 @@
 console.log("Hello, World!");
-
-const botonesCapsulas = document.querySelectorAll('.selector-capsulas button');
-const imagenProducto = document.getElementById('imagen-producto');
-const nombreProducto = document.getElementById('nombre-producto');
-const precioProducto = document.getElementById('precio-producto');
-
 let menuMobile = document.getElementById("menu-mobile");
-      let ham_menu = document.getElementById("ham_menu");
+let ham_menu = document.getElementById("ham_menu");
 
-      menuMobile.addEventListener("click", function () {
-        ham_menu.classList.toggle("hidden");
-      });
-      
-//Acordion
-
+menuMobile.addEventListener("click", function () {
+    ham_menu.classList.toggle("hidden");
+});
